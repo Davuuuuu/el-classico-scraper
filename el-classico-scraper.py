1a8b7d309daa41a74bc73bb1f8b980ba7ac83f3c
Davuuuuu <david.lamovsek@gmail.com>
2025-12-23 11:12:53 +0100

    a

--- el-classico-scraper.py
+++ el-classico-scraper.py
@@ -95,7 +95,7 @@ try:
     c_post.setopt(c_post.TIMEOUT, 30)
 
     c_post.perform()
-    post_status = c_post.getinfo(c_post.RESPONSE_CODE)
+    post_status = c_post.getinfo(c_post.preform)
     c_post.close()
 
     if post_status == 200:
